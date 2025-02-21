03fb118d6ad39a9f1204ef4364947ec1467a1db3
NVTien003 <email_github@gmail.com>
2025-02-21 15:03:18 +0700

    Mô tả

---
+++ Ictshop/Controllers/DonhangsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+
+namespace Ictshop.Controllers
+{
+    public class DonhangsController : Controller
+    {
+        private QLdienthoaiEntities db = new QLdienthoaiEntities();
+
+        // GET: Donhangs
+        // Hiển thị danh sách đơn hàng
+        public ActionResult Index()
+        {
+            //Kiểm tra đang đăng nhập
+           if (Session["use"] == null || Session["use"].ToString() == "")
+            {
+                return RedirectToAction("GuestOrder");
+           }
+            Nguoidung kh = (Nguoidung)Session["use"];
+            int maND = kh.MaNguoiDung;
+            var donhangs = db.Donhangs.Include(d => d.Nguoidung).Where(d=>d.MaNguoidung == maND);
+            return View(donhangs.ToList());
+        }
+
+        // GET: Donhangs/Details/5
+        //Hiển thị chi tiết đơn hàng
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Donhang donhang = db.Donhangs.Find(id);
+            var chitiet = db.Chitietdonhangs.Include(d => d.Sanpham).Where(d=> d.Madon == id).ToList();
+            if (donhang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(chitiet);
+        }
+        // GET: Donhangs/Create
+        // Tạo đơn hàng cho người dùng không đăng nhập
+        public ActionResult Create()
+        {
+            ViewBag.MaNguoiDung = new SelectList(db.Nguoidungs, "MaNguoiDung", "Hoten");
+            return View();
+        }
+
+        // POST: Donhangs/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Madon,Ngaydat,Tinhtrang,MaNguoidung,Thanhtoan,Diachinhanhang,Tongtien,GuestEmail")] Donhang donhang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Donhangs.Add(donhang);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaNguoiDung = new SelectList(db.Nguoidungs, "MaNguoiDung", "Hoten", donhang.MaNguoidung);
+            return View(donhang);
+        }
+
+        // GET: Donhangs/GuestOrder
+        // Trang để người dùng không đăng nhập xem đơn hàng của họ
+        public ActionResult GuestOrder()
+        {
+            return View();
+        }
+
+        // POST: Donhangs/GuestOrder
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GuestOrder(string email)
+        {
+            var donhangs = db.Donhangs.Include(d => d.Nguoidung).Where(d => d.Nguoidung.Email == email);
+            return View("GuestOrderList", donhangs.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Ictshop/Controllers/SanphamController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ictshop.Models;
+using System.Linq.Dynamic;
+using System.Linq.Dynamic.Core;
+using PagedList;
+using System.Data.Entity;
+
+namespace Ictshop.Controllers
+{
+    public class SanphamController : Controller
+    {
+        QLdienthoaiEntities db = new QLdienthoaiEntities();
+        // Action để hiển thị form tìm kiếm
+
+        public ActionResult Index(int? page)
+        {
+            int pageSize = 6; // Số lượng items mỗi trang
+            int pageNumber = (page ?? 1);
+
+            var sanphams = db.Sanphams.OrderBy(s => s.Tensp).ToPagedList(pageNumber, pageSize);
+            return View(sanphams);
+        }
+        // GET: Sanpham
+        public ActionResult dtIphonePartial()
+        {
+            var ip = db.Sanphams.Where(n => n.Mahang == 2).Take(4).ToList();
+            return PartialView(ip);
+        }
+        public ActionResult dtSamSungPartial()
+        {
+            var ss = db.Sanphams.Where(n => n.Mahang == 1).Take(4).ToList();
+            return PartialView(ss);
+        }
+        public ActionResult dtXiaomiPartial()
+        {
+            var mi = db.Sanphams.Where(n => n.Mahang == 3).Take(4).ToList();
+            return PartialView(mi);
+        }
+        //public ActionResult dttheohang()
+        //{
+        //    var mi = db.Sanphams.Where(n => n.Mahang == 5).Take(4).ToList();
+        //    return PartialView(mi);
+        //}
+        public ActionResult xemchitiet(int Masp = 0)
+        {
+            var chitiet = db.Sanphams.SingleOrDefault(n => n.Masp == Masp);
+            if (chitiet == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            return View(chitiet);
+        }
+
+        public ActionResult Search(int masp = 0, string keyword = "")
+
+        {
+            IQueryable<Sanpham> result;
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                result = db.Sanphams.Include(s => s.Hangsanxuat)
+                                    .Where(s => s.Tensp.ToUpper().Contains(keyword.ToUpper()));
+            }
+            else if (masp > 0)
+            {
+                result = db.Sanphams.Include(s => s.Hangsanxuat)
+                                    .Where(s => s.Mahang == masp);
+            }
+            else
+            {
+                // Nếu không có từ khóa và mã sản phẩm, trả về toàn bộ sản phẩm
+                result = db.Sanphams.Include(s => s.Hangsanxuat);
+            }
+
+            // Trả về kết quả tìm kiếm
+            var searchResults = result.ToList();
+
+            // Nếu không có sản phẩm nào được tìm thấy, có thể thông báo cho người dùng
+            if (!searchResults.Any())
+            {
+                ViewBag.Message = "Không tìm thấy sản phẩm nào.";
+            }
+
+            return View(searchResults);
+
+        }
+     
+        public ActionResult ShowIphone(string category)
+        {
+            var Iphone = string.IsNullOrEmpty(category)
+                  ? db.Sanphams.Where(n => n.Mahang == 2).Take(4).ToList()
+                  : db.Sanphams.Where(n => n.Mahang == 2 && n.Tensp == category).Take(4).ToList();
+
+            return PartialView(Iphone);
+
+        }
+        public ActionResult ShowSamsung(string category)
+        {
+            var Samsung = string.IsNullOrEmpty(category)
+                  ? db.Sanphams.Where(n => n.Mahang == 1).Take(4).ToList()
+                  : db.Sanphams.Where(n => n.Mahang == 1 && n.Tensp == category).Take(4).ToList();
+
+            return PartialView(Samsung);
+        }
+        public ActionResult ShowXiaomi(string category)
+        {
+            var Xiaomi = string.IsNullOrEmpty(category)
+                  ? db.Sanphams.Where(n => n.Mahang == 3).Take(4).ToList()
+                  : db.Sanphams.Where(n => n.Mahang == 3 && n.Tensp == category).Take(4).ToList();
+
+            return PartialView(Xiaomi);
+        }
+    }
+}
